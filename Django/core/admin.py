d623e670b434917cd4b5566f8a73d3587cf9b9be
phuckhoa33 <phuckhoa81@gmail.com>
2023-09-30 01:31:23 +0700

    Config models

--- Django/core/admin.py
+++ Django/core/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import SearchQuery, SearchResult, AnalyticData
 
 # Register your models here.
+admin.register(SearchQuery, SearchResult, AnalyticData)
--- Django/core/models.py
+++ Django/core/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+
+class SearchQuery(models.Model):
+    pass
+
+
+class SearchResult(models.Model):
+    pass
+
+
+class AnalyticData(models.Model):
+    pass
