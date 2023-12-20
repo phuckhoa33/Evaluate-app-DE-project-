5e97d4d1c2b0235d78c17288c65b2efbce96cb97
Admin <phuckhoa33>
2023-12-21 00:10:41 +0700

    Config login and register page and many page for basic website

--- Django/Django/urls.py
+++ Django/Django/urls.py
@@ -1,11 +1,9 @@
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
-
-from core.views import index, contact
+from django.urls import path, include
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('contact/', contact, name='contact'),
-    path('admin/', admin.site.urls),
-]
+    path('', include('core.urls')),
+    path('admin/', admin.site.urls)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- Django/core/views.py
+++ Django/core/views.py
@@ -1,11 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
 
+from .forms import SignupForm
 
 def index(request):
-    return render(request, 'core/index.html')
+    
 
+    return render(request, 'core/index.html', {
+       
+    })
 
 def contact(request):
     return render(request, 'core/contact.html')
+
+def signup(request):
+    if request.method == 'POST':
+        form = SignupForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            return redirect('/login/')
+    else:
+        form = SignupForm()
+
+    return render(request, 'core/signup.html', {
+        'form': form
+    })
